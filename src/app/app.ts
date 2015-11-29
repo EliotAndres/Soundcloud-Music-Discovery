075f2b7a0012336200b7b3ed1afd36775f4d73d4
eliot.andres <eliot.andres@eleves.enpc.fr>
2015-11-29 13:08:49 +0100

    Add app source

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 /*
  * Angular 2 decorators and services
  */
-import {Directive, Component, View, ElementRef} from 'angular2/angular2';
+import {Directive, Component, View, ElementRef, NgStyle} from 'angular2/angular2';
 import {RouteConfig, Router} from 'angular2/router';
-import {Http, Headers} from 'angular2/http';
+import {Http, Headers, Response} from 'angular2/http';
 
 /*
  * Angular Directives
@@ -11,134 +11,103 @@ import {Http, Headers} from 'angular2/http';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 
+/*
+ * Services and components
+ */
+import {MusicPlayerService} from './service/music-player';
+import {SearchBar} from "./components/search-bar";
+import {StatusBar} from "./components/status-bar";
 
 /*
- * Directive
- * XLarge is a simple directive to show how one is made
+ * Styles
  */
-@Directive({
-  selector: '[x-large]' // using [ ] means selecting attributes
-})
-export class XLarge {
-  constructor(element: ElementRef) {
-    // simple DOM manipulation to set font size to x-large
-    // `nativeElement` is the direct reference to the DOM element
-    element.nativeElement.style.fontSize = 'x-large';
-  }
-}
+require('./styles/app.scss');
 
+/*
+ * Config
+ */
+var config = require('./config/config');
 
 /*
  * App Component
  * Top Level Component
  */
 @Component({
-  // The selector is what angular internally uses
-  // for `document.querySelectorAll(selector)` in our index.html
-  // where, in this case, selector is the string 'app'
   selector: 'app', // <app></app>
-  // We need to tell Angular's compiler which directives are in our template.
-  // Doing so will allow Angular to attach our behavior to an element
-  directives: [ CORE_DIRECTIVES, FORM_DIRECTIVES, ROUTER_DIRECTIVES, XLarge ],
-  // Our list of styles in our component. We may add more to compose many styles together
-  styles: [`
-    .title {
-      font-family: Arial, Helvetica, sans-serif;
-    }
-    main {
-      padding: 1em;
-    }
-  `],
+  directives: [SearchBar, NgStyle, StatusBar],
+  providers: [MusicPlayerService],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   template: `
-  <header>
-    <h1 class="title">Hello {{ title }}</h1>
-  </header>
+  <div>
+    <search-bar (select)="useTrack($event)"></search-bar>
 
-  <main>
-    Your Content Here
-    <div>
+    <img class="logo" src="img/logo.png" />
 
-      <input type="text" [value]="title" (input)="title = $event.target.value" autofocus>
-      <!--
-        Rather than wiring up two-way data-binding ourselves
-        we can use Angular's [(ng-model)] syntax
-        <input type="text" [(ng-model)]="title">
-      -->
+    <div class="headline" *ng-if="currentTrack.title">
+      Tracks in playlists containing {{currentTrack.user.username}} - {{currentTrack.title}} :
     </div>
 
-    <pre>this.title = {{ title | json }}</pre>
-    <pre>this.data = {{ data | json }}</pre>
+    <div class="loader-wrapper" *ng-if="loading">
+      <div class="loader">
+      </div>
+    </div>
 
-  </main>
+    <ul *ng-if="tracks" class="venn-result">
+       <li class="track" *ng-for="#track of tracks">
+          <div class="track_background-image"  [ng-style]="{'background-image': 'url(' + track.artwork_url + ')'}">
+          </div>
+          <div class="track_content" [ng-class]="{active: track.id == getCurrentTrack().id}" (click)="playTrack(track)">
+            {{ track.user.username }} {{ track.title }} ({{ track.total_count }})
+          </div>
+      </li>
+    </ul>
 
-  <footer x-large>
-    WebPack Angular 2 Starter by <a href="https://twitter.com/AngularClass">@AngularClass</a>
-  </footer>
+  </div>
+  <status-bar></status-bar>
   `
 })
+
 export class App {
-  // These are member type
-  title: string;
-  data: Array<any> = []; // default data
-  // TypeScript public modifiers
-  constructor(public http: Http) {
-    this.title = 'Angular 2';
+  tracks;
+  currentTrack = {user:{}};
+  loading:boolean = false;
+
+  constructor(public musicPlayerService: MusicPlayerService, public http: Http) {
+    this.musicPlayerService.init();
   }
 
-  onInit() {
-    // Our API
-    // Before you start the app, run these commands in another process:
-    //
-    // - npm run express-install
-    // - npm run express
-    //
-    // This will start a process that will listen for requests on port 3001
-
-    const BASE_URL = 'http://localhost:3001';
-    const TODO_API_URL = '/api/todos';
-    const JSON_HEADERS = new Headers();
-
-    JSON_HEADERS.append('Accept', 'application/json');
-    JSON_HEADERS.append('Content-Type', 'application/json');
-
-    this.http
-      .get(BASE_URL + TODO_API_URL, {
-        headers: JSON_HEADERS
-      })
+  useTrack(event) {
+    if (typeof event.track === 'undefined') return;
+    this.currentTrack = event.track;
+    this.loading = true;
+    var url = config.apiUrl + '/soundcloud?trackIds=' + event.track.id ;
+    this.musicPlayerService.clearPlaylist();
+    this.http.get(url)
+      .map((res: Response) => res.json())
       .subscribe(
-        // onNext callback
-        data => this.serverData(data),
-        // onError callback
-        err  => this.errorMessage(err),
-        // onComplete callback
-        ()   => console.log('complete')
-      );//end http
-
+        data => this.addTracksToPlaylist(data),
+        (err) =>  {
+          this.tracks = {};
+          console.log('There has been an error', err);
+        },
+        () => this.loading = false
+      );
   }
 
-  serverData(data) {
-    console.log('data', data);
-    this.data = data;
-  }//serverData
+  addTracksToPlaylist(data){
+    this.tracks = data;
+    for (var i = 0; i < data.length; i ++) {
+      this.musicPlayerService.addToPlaylist(data[i]);
+    }
+  }
 
-  errorMessage(err) {
-    console.info(`${'\n'
-      } // You must run these commands in another process for the Http API to work  ${'\n'
-      } npm run express-install ${'\n'
-      } npm run express
-    `);
-  }//errorMessage
+  playTrack(track) {
+    this.musicPlayerService.playTrack(track);
+  }
+
+  getCurrentTrack() {
+    if (typeof this.musicPlayerService.getCurrentTrack() === 'undefined') return {user:{}};
+    else return this.musicPlayerService.getCurrentTrack();
+  }
 
 }
-
-
-
-/*
- * Please review the examples/ folder for more angular app examples
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * you can change the `entry` in webpack.config to quickly view the examples
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- * or via chat on Gitter at https://gitter.im/AngularClass/angular2-webpack-starter
- */
---
+++ src/app/service/music-player.ts
@@ -0,0 +1,176 @@
+import {Injectable, bind} from 'angular2/angular2';
+import {Http} from 'angular2/http';
+
+var config = require('../config/config')
+declare var soundManager:any;
+
+@Injectable()
+export class MusicPlayerService {
+
+  private currentTrack;
+  private isPlaying: boolean;
+  private trackProgress = 0;
+  private trackList = [];
+  private http;
+  private currentPosition;
+
+	constructor(http: Http) {
+		this.http = http;
+	}
+
+	init(){
+	  this.initSoundManager();
+	}
+
+  playTrack(track) {
+
+    if (typeof this.getCurrentTrack() !== 'undefined') this.stop();
+
+    soundManager.createSound({
+      id: track.id, // optional: provide your own unique id
+      url: 'http://api.soundcloud.com/tracks/'+ track.id + '/stream?client_id=' + config.soundcloudKey
+    });
+
+    soundManager.play(track.id);
+
+    this.setCurrentTrack(track);
+    this.setPlayingStatus(true);
+  }
+
+  play() {
+    soundManager.play(this.getCurrentTrack().id);
+    this.setPlayingStatus(true);
+  }
+
+  pause() {
+    soundManager.pause(this.getCurrentTrack().id);
+    this.setPlayingStatus(false);
+  }
+
+  stop() {
+      this.pause();
+      this.resetProgress();
+      soundManager.stopAll();
+      soundManager.unload(this.getCurrentTrack());
+      soundManager.destroySound(this.getCurrentTrack().id);
+  }
+
+  resetProgress() {
+    this.trackProgress = 0;
+  }
+
+  togglePlay() {
+    if(this.isPlaying) this.pause();
+    else  this.play();
+  }
+
+  setPlayingStatus(playing: boolean) {
+    this.isPlaying = playing;
+  }
+
+  getPlayingStatus(): boolean {
+    return this.isPlaying;
+  }
+
+  setCurrentTrack(track) {
+    this.currentTrack = track;
+  }
+
+  getCurrentTrack() {
+    if (typeof this.currentTrack === 'undefined') return {user:{}};
+    return this.currentTrack;
+  }
+
+  getCurrentPosition() {
+    return this.currentPosition;
+  }
+
+  setCurrentPosition(position) {
+    this.currentPosition = position;
+  }
+
+  getDuration(position) {
+    if (typeof this.getCurrentTrack() === 'undefined') return 0;
+
+    var sound = soundManager.getSoundById(this.getCurrentTrack().id);
+    return sound.durationEstimate;
+  }
+
+  nextTrack(){
+    if (typeof this.getCurrentTrack() === 'undefined'
+    || this.trackList.length < 1)
+      return;
+
+    this.stop();
+
+    console.log(this.trackList.indexOf(this.getCurrentTrack()));
+
+    var nextPosition = this.trackList.indexOf(this.getCurrentTrack()) + 1;
+
+    if(this.trackList.length > nextPosition) {
+      this.playTrack(this.trackList[nextPosition]);
+    }else{
+      this.playTrack(this.trackList[0]);
+    }
+  }
+
+  addToPlaylist(track) {
+    this.trackList.push(track);
+  }
+
+  clearPlaylist() {
+    this.trackList = [];
+  }
+
+  initSoundManager(){
+
+    var _this = this;
+    soundManager.setup({
+            preferFlash: false, // prefer 100% HTML5 mode, where both supported
+            debugMode: false, // enable debugging output ($log.debug() with HTML fallback)
+            useHTML5Audio: true,
+            ontimeout: function() {
+              alert('SM2 failed to start. Flash missing, blocked or security error?');
+            },
+            defaultOptions: {
+              // set global default volume for all sound objects
+              autoLoad: false, // enable automatic loading (otherwise .load() will call with .play())
+              autoPlay: false, // enable playing of file ASAP (much faster if "stream" is true)
+              from: null, // position to start playback within a sound (msec), see demo
+              loops: 1, // number of times to play the sound. Related: looping (API demo)
+              multiShot: false, // let sounds "restart" or "chorus" when played multiple times..
+              multiShotEvents: false, // allow events (onfinish()) to fire for each shot, if supported.
+              onid3: null, // callback function for "ID3 data is added/available"
+              onstop: null, // callback for "user stop"
+              onfailure: function() {
+                _this.nextTrack();
+              },
+              onpause: null, // callback for "pause"
+              onplay: null, // callback for "play" start
+              onresume: null, // callback for "resume" (pause toggle)
+              position: null, // offset (milliseconds) to seek to within downloaded sound.
+              pan: 0, // "pan" settings, left-to-right, -100 to 100
+              stream: true, // allows playing before entire file has loaded (recommended)
+              to: null, // position to end playback within a sound (msec), see demo
+              type: 'audio/mp3', // MIME-like hint for canPlay() tests, eg. 'audio/mp3'
+              usePolicyFile: false, // enable crossdomain.xml request for remote domains (for ID3/waveform access)
+              volume: 90, // self-explanatory. 0-100, the latter being the max.
+              whileloading: function() {
+              },
+              whileplaying: function() {
+                _this.currentPosition = ((this.position / this.duration) * 100);
+              },
+              onfinish: function() {
+                _this.nextTrack();
+              },
+              onload: function() {
+                if (this.readyState == 2){
+                   console.log('Error loading track');
+                   _this.nextTrack();
+                 }
+              }
+            }
+          });
+  }
+
+}
